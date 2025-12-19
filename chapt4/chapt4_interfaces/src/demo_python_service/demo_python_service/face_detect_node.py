bb680ab4966e55d453ee47342593c23a981152db
loxfran <1779228995@qq.com>
2025-12-19 10:22:32 +0800

    params first try

--- chapt4/chapt4_interfaces/src/demo_python_service/demo_python_service/face_detect_node.py
+++ chapt4/chapt4_interfaces/src/demo_python_service/demo_python_service/face_detect_node.py
@@ -6,7 +6,7 @@ from cv_bridge import CvBridge
 import cv2
 import face_recognition
 import time
-
+from rcl_interfaces.msg import SetParametersResult
 
 class FaceDetectionNode(Node):
     def __init__(self):
@@ -14,8 +14,22 @@ class FaceDetectionNode(Node):
         self.bridge = CvBridge()
         self.srv = self.create_service(FaceDetector, '/face_detect', self.face_detect_callback)
         self.demo_image_path = get_package_share_directory('demo_python_service') + '/resource/demo.jpeg'
-        self.upsample_times = 1
-        self.model = 'hog'
+        self.declare_parameter('upsample_times', 1)
+        self.declare_parameter('model', 'hog')
+        self.upsample_times = self.get_parameter('upsample_times').get_parameter_value().integer_value
+        self.model = self.get_parameter('model').get_parameter_value().string_value
+        self.add_on_set_parameters_callback(self.params_callback)
+        # self.upsample_times = 1
+        # self.model = 'hog'
+
+    def params_callback(self, params):
+        for param in params:
+            self.get_logger().info(f'params_callback: {param.name}->{param.value}')
+            if param.name == 'upsample_times':
+                self.upsample_times = param.value
+            elif param.name == 'model':
+                self.model = param.value
+        return SetParametersResult(successful=True)
 
     def face_detect_callback(self, request, response):
         if request.image.data:
